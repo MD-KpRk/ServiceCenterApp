69383370b6d551df78e73810121d79e31383e807
MD-KpRk <griferad@gmail.com>
2025-11-15 23:45:22 +0300

    Relay Commands Update

--- ServiceCenterApp/ViewModels/RelayCommand.cs
+++ ServiceCenterApp/ViewModels/RelayCommand.cs
@@ -7,10 +7,10 @@ using System.Windows.Input;
 
 namespace ServiceCenterApp.ViewModels
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool> _canExecute;
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
 
         public event EventHandler? CanExecuteChanged
         {
@@ -18,13 +18,22 @@ namespace ServiceCenterApp.ViewModels
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
         {
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
-        public void Execute(object parameter) => _execute(parameter);
+        public bool CanExecute(object parameter)
+        {
+            if (parameter == null) return false;
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter == null) return;
+            _execute((T)parameter);
+        }
     }
 }
