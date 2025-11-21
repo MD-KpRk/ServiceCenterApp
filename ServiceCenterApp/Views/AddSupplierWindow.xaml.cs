45f53a2328b8d06069ae542d95e3bfec146d5f12
MD-KpRk <griferad@gmail.com>
2025-11-21 22:56:23 +0300

    AddSupplierWindow Added

---
+++ ServiceCenterApp/Views/AddSupplierWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ServiceCenterApp.Models;
+using System.Windows;
+
+namespace ServiceCenterApp.Views
+{
+    public partial class AddSupplierWindow : Window
+    {
+        public Supplier NewSupplier { get; private set; }
+
+        public AddSupplierWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTb.Text) || string.IsNullOrWhiteSpace(ContactTb.Text))
+            {
+                MessageBox.Show("Название и Контакты обязательны.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            NewSupplier = new Supplier
+            {
+                Name = NameTb.Text,
+                Contacts = ContactTb.Text,
+                Details = DetailsTb.Text
+            };
+
+            DialogResult = true;
+        }
+    }
+}
