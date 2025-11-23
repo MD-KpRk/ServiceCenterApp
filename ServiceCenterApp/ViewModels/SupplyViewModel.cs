b54b4bb51f9ad67368a4056e0c9b7cf8e2d89165
MD-KpRk <griferad@gmail.com>
2025-11-23 15:43:45 +0300

    Added supply window for supply logic

---
+++ ServiceCenterApp/ViewModels/SupplyViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ServiceCenterApp.ViewModels
+{
+    public class SupplyViewModel : BaseViewModel
+    {
+        public int IncomingQuantity { get; set; }
+        public decimal IncomingPrice { get; set; }
+
+        public ICommand ConfirmCommand { get; }
+
+        public SupplyViewModel()
+        {
+            ConfirmCommand = new RelayCommand(Confirm);
+        }
+
+        private void Confirm()
+        {
+            if (IncomingQuantity <= 0)
+            {
+                MessageBox.Show("Количество должно быть больше 0.");
+                return;
+            }
+            if (IncomingPrice < 0)
+            {
+                MessageBox.Show("Цена не может быть отрицательной.");
+                return;
+            }
+
+            // Закрываем окно с результатом True
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.DataContext == this)
+                {
+                    window.DialogResult = true;
+                    window.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ServiceCenterApp/Views/SupplyWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ServiceCenterApp.ViewModels;
+using System.Windows;
+
+namespace ServiceCenterApp.Views
+{
+    public partial class SupplyWindow : Window
+    {
+        public SupplyViewModel ViewModel => (SupplyViewModel)DataContext;
+
+        public SupplyWindow()
+        {
+            InitializeComponent();
+            DataContext = new SupplyViewModel();
+        }
+    }
+}
