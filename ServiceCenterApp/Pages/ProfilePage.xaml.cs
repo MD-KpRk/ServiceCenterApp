5df82c58270fbc216710b446fe132da6cfbb5efe
MD-KpRk <griferad@gmail.com>
2025-11-24 17:14:27 +0300

    Added Profile Page

---
+++ ServiceCenterApp/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ServiceCenterApp.ViewModels;
+using System.Windows.Controls;
+
+namespace ServiceCenterApp.Pages
+{
+    public partial class ProfilePage : Page
+    {
+        public ProfilePage(ProfileViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
